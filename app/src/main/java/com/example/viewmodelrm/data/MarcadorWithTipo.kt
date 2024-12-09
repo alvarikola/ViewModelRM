c58c023ad378d59ec8f192ed791696dfb871f131
alvarikola <alvaroe484@gmail.com>
2024-12-09 19:21:02 +0000

    realizado MarcadorWithTipo

--- app/src/main/java/com/example/viewmodelrm/data/MarcadorWithTipo.kt
+++ app/src/main/java/com/example/viewmodelrm/data/MarcadorWithTipo.kt
@@ -1,4 +1,13 @@
 package com.example.viewmodelrm.data
 
-class MarcadorWithTipo {
-}
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class MarcadorWithTipo(
+    @Embedded val marcador: Marcador,
+    @Relation(
+        parentColumn = "idTipoMarcadorOwner",
+        entityColumn = "idTipoMarcador"
+    )
+    val tiposMarcadores: List<TipoMarcador>
+)
