c020b494bea33fb98f3f593eea5fe97fda7c4f4a
alvarikola <alvaroe484@gmail.com>
2024-12-09 19:14:49 +0000

    realizado MarcadorDao

--- app/src/main/java/com/example/viewmodelrm/data/MarcadorDao.kt
+++ app/src/main/java/com/example/viewmodelrm/data/MarcadorDao.kt
@@ -1,4 +1,18 @@
 package com.example.viewmodelrm.data
 
-class MarcadorDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+
+
+@Dao
+interface MarcadorDao {
+
+    @Insert
+    suspend fun insertTarea(marcador: Marcador)
+
+
+    @Query("SELECT * FROM MARCADORES")
+    suspend fun getAllMarcadoresAndTipos(): List<MarcadorWithTipo>
 }
