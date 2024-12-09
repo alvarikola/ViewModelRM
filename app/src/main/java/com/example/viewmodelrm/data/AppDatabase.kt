603681fa903d4787ddf7db0e8e530e4c924e96d5
alvarikola <alvaroe484@gmail.com>
2024-12-09 19:26:58 +0000

    realizado AppDatabase

--- app/src/main/java/com/example/viewmodelrm/data/AppDatabase.kt
+++ app/src/main/java/com/example/viewmodelrm/data/AppDatabase.kt
@@ -1,4 +1,25 @@
 package com.example.viewmodelrm.data
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Marcador::class, TipoMarcador::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun marcadorDao(): MarcadorDao
+    abstract fun tipoMarcadorDao(): TipoMarcadorDao
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "marcadores_database" ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
