c6365feb361d8f69d0c91cb19ec58b6eefdcf44c
alvarikola <alvaroe484@gmail.com>
2024-12-09 19:10:15 +0000

    realizado Marcador

--- app/src/main/java/com/example/viewmodelrm/data/Marcador.kt
+++ app/src/main/java/com/example/viewmodelrm/data/Marcador.kt
@@ -1,4 +1,16 @@
 package com.example.viewmodelrm.data
 
-class Marcador {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "Marcadores")
+data class Marcador(
+    @PrimaryKey(autoGenerate = true) val idMarcador: Int = 0,
+    @ColumnInfo(name = "Título") val tituloMarcador: String,
+    @ColumnInfo(name = "Descripción") val descripcionMarcador: String?,
+    @ColumnInfo(name = "CoordenadaX") val coordenadaX: Double,
+    @ColumnInfo(name = "CoordenadaY") val coordenadaY: Double,
+    val idTipoMarcadorOwner: Int
+)
