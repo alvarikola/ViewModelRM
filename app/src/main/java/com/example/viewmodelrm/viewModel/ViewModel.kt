77a9db6c981d6222f1b4ea7c1ee440df51d37602
alvarikola <alvaroe484@gmail.com>
2024-12-10 13:54:48 +0000

    indentacion

--- app/src/main/java/com/example/viewmodelrm/viewModel/ViewModel.kt
+++ app/src/main/java/com/example/viewmodelrm/viewModel/ViewModel.kt
@@ -7,5 +7,6 @@ import kotlinx.coroutines.flow.Flow
 
 class MarcadorViewModel(private val marcadorDao: MarcadorDao) : ViewModel() {
 
-    val marcadoresWithTipo: Flow<List<MarcadorWithTipo>> = marcadorDao.getAllMarcadoresAndTipos()
+    val marcadoresWithTipo: Flow<List<MarcadorWithTipo>> =
+        marcadorDao.getAllMarcadoresAndTipos()
 }
